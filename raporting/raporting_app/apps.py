3041a35f167e9656f6b317cbb9553dd74e331099
adambajguz <adam.bajguz@gmail.com>
2018-04-19 18:33:02 +0200

    Deleted

--- raporting/raporting_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RaportingAppConfig(AppConfig):
-    name = 'raporting_app'
--- raporting/raporting_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
- 
-class Workbooks(models.Model):
-    workbook_name = models.CharField(max_length=30)
-    sheet_name = models.CharField(max_length=30)
-    data = models.TextField()
