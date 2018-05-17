3f607e7b409b5384288f5dd0345b5dcd60d0828a
adambajguz <adam.bajguz@gmail.com>
2018-05-17 23:53:34 +0200

    UI improvements

--- ReportingApp/reports/templatetags/custom_tags.py
+++ ReportingApp/reports/templatetags/custom_tags.py
@@ -2,8 +2,12 @@ from django import template
 register = template.Library()
 
 
-@register.filter
+@register.filter(name='index')
 def index(List, i):
     if i < len(List):
         return List[int(i)]
     return ''
+
+@register.filter(name='addclass')
+def addclass(field, css):
+   return field.as_widget(attrs={"class":css})
