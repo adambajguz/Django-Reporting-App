188286dc24d56589fdf6c71828cb64c4337175e4
Kiel97 <kkielek97@gmail.com>
2018-06-04 20:14:30 +0200

    Added pythonanywhere web app as an allowed hosts

--- ReportingApp/ReportingApp/settings.py
+++ ReportingApp/ReportingApp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'd#t3un_+)w!qj*fe!geznt^9kgdj@j=-nnm^$ae4v+yk#pvkzd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rswtasheetpro.pythonanywhere.com']
 
 
 # Application definition
